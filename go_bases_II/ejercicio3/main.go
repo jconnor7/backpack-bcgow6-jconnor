0ea4c29dc6b9c0fa59d02ae47c1792c431190718
jconnor7 <juan.calle@mercadolibre.com.co>
2022-10-24 00:04:23 -0500

    Modifies con constants usages and docstring

--- go_bases_II/ejercicio3/main.go
+++ go_bases_II/ejercicio3/main.go
@@ -4,17 +4,6 @@ import (
 	"fmt"
 )
 
-/*Ejercicio 3 - Calcular salario
-
-Una empresa marinera necesita calcular el salario de sus empleados basándose en la cantidad de horas trabajadas por mes y la categoría.
-
-	1. Si es categoría C, su salario es de $1.000 por hora
-	2. Si es categoría B, su salario es de $1.500 por hora más un %20 de su salario mensual
-	3. Si es de categoría A, su salario es de $3.000 por hora más un %50 de su salario mensual
-
-Se solicita generar una función que reciba por parámetro la cantidad de minutos trabajados por mes y la categoría, y que devuelva su salario.
-*/
-
 const (
 	catA = "A"
 	catB = "B"
